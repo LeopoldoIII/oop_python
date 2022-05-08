098ed8cd595ae176f3c7a15992833a3b1c70b432
LeopoldoIII <nathan.rx@gmail.com>
2022-05-07 23:28:38 -0500

    classmethod example

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+from asyncore import read
+import csv
+
 
 class Item:
-    pay_rate = 0.8 # The pay rate after 20% discount
+    pay_rate = 0.8  # The pay rate after 20% discount
     all = []
 
     def __init__(self, name: str, price: float, quantity=0):
 
         # Run validations to the received arguments
+        assert quantity >= 0, (
+            f"Quantity {quantity} is not greater or equal to zero")
         assert price >= 0, (f"Price {price} is not greater than zero")
-        assert quantity >= 0, (f"Quantity {quantity} is not greater or equal to zero")
 
         self.name = name
         self.price = price
@@ -15,7 +19,6 @@ class Item:
 
         # Actions to execute
         Item.all.append(self)
-    
 
     def calculate_total_price(self):
         return self.price * self.quantity
@@ -23,18 +26,23 @@ class Item:
     def apply_discount(self):
         self.price = self.price * self.pay_rate
 
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+
+        for item in items:
+            Item(
+                name=item.get('name'),
+                price=float(item.get('price')),
+                quantity=float(item.get('quantity'))
+            )
+
+
     def __repr__(self):
         return (f"Item('{self.name}', {self.price}, {self.quantity})")
 
-item1 = Item("Phone", 100, 1) 
-item2 = Item("Laptop", 1000, 3)
-item3 = Item("Cable", 10, 5)
-item4 = Item("Mouse", 50,5)
-item5 = Item("Keyboard", 75, 5)
 
-
-
-
-
-
-# print(item1.calculate_total_price())
+Item.instantiate_from_csv()
+print(Item.all)
